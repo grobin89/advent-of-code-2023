75301192ba4e9d23326072a7e106a64a648e9225
grobin89 <grobin89@gmail.com>
2024-02-03 16:41:13 +1100

    day 06-2 solution

--- src/workspaces/WorkspaceDay06.kt
+++ src/workspaces/WorkspaceDay06.kt
@@ -5,16 +5,25 @@ import domain.Race
 class WorkspaceDay06 : Workspace2<List<Race>, Long> {
     override fun part1(input: List<Race>): Long {
         return input
-                .map { race ->
-                    (0L..race.time)
-                            .map { buttonHoldTime -> buttonHoldTime * (race.time - buttonHoldTime) }
-                            .filter { it > race.distance }
-                            .size.toLong()
-                }
+                .map { race -> findWaysToWin(race) }
                 .fold(1) { acc, l -> l * acc }
     }
 
     override fun part2(input: List<Race>): Long {
-        TODO("Not yet implemented")
+        val race = parse2(input)
+        return findWaysToWin(race)
+    }
+
+    private fun parse2(input: List<Race>): Race {
+        val time = input.map { it.time }.joinToString("").toLong()
+        val distance = input.map { it.distance }.joinToString("").toLong()
+        return Race(time, distance)
+    }
+
+    private fun findWaysToWin(race: Race): Long {
+        return (0L..race.time)
+            .map { buttonHoldTime -> buttonHoldTime * (race.time - buttonHoldTime) }
+            .filter { it > race.distance }
+            .size.toLong()
     }
 }
